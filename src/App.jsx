13d0af2a38f42328a89c4a035917ba556a2d4df9
Vale Eseve <vale.eseve@gmail.com>
2026-01-16 22:26:18 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+      <button className='btn btn-primary'>Test</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/PrintsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PrintsPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PrintsPage
