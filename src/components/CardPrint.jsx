39227170f4803eaf859df34b7f98b42cefa23d02
Vale Eseve <vale.eseve@gmail.com>
2026-01-17 20:47:38 -0300

    CardPrint con nombre de artista

--- src/components/CardPrint.jsx
+++ src/components/CardPrint.jsx
@@ -1,14 +1,15 @@
 import './CardPrint.css'
 
-const CardPrint = (props) => {
+const CardPrint = ({print, user}) => {
+    console.log("User =>" + user)
     return (
         <article>
             <div className="card" style={{width: "18rem"}}>
-                <img src={props.print.img} className="card-img-top p-2 card-img-fixed" alt="..."/>
+                <img src={print.img} className="card-img-top p-2 card-img-fixed" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">{props.print.name}</h5>
-                        
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <h5 className="card-title">{print.name}</h5>
+                        <h6>Art by {user?.name}</h6>
+                        <a href="#" className="btn btn-primary"><i class="fa-solid fa-cart-arrow-down"></i></a>
                     </div>
             </div>
         </article>
--- src/components/FeaturedPrints.jsx
+++ src/components/FeaturedPrints.jsx
@@ -2,30 +2,50 @@ import { useContext, useEffect, useState } from 'react'
 import { getFeatured } from "../utils/featured"
 import { PrintContext } from '../context/PrintContext'
 import CardPrint from './CardPrint'
+import { UserContext } from '../context/UserContext'
 
 const FeaturedPrints = () => {
     const { prints } = useContext(PrintContext)
+    const { users } = useContext(UserContext)
     const [featPrints, setFeatPrints] = useState([]);
+    const [printsWithArtist, setPrintsWithArtist] = useState([])
+
+
+
 
     useEffect(() => {
-        setFeatPrints(getFeatured(prints));
-        console.log("Prints en featured"+featPrints)
-    }, [prints]);
+        if (!prints.length || !users.length) return;
+
+        const featured = getFeatured(prints);
+
+        setPrintsWithArtist(
+            featured.map(print => {
+                const foundUser = users.find(u => String(u.id) === String(print.userId));
+                console.log(`Print ${print.id} - userId: ${print.userId} - Found user:`, foundUser);
+                return {
+                    ...print,
+                    user: foundUser
+                }
+            })
+        );
+    }, [prints, users]);
+
+
 
 
     return (
         <div>
             <br />
             <h2 className='text-center'>Featured Prints</h2>
-        <section className='container-fluid d-flex justify-content-center gap-3'>
-            {featPrints.map(print => (
-                <CardPrint key={print.id} print={print} />
-            ))}
+            <section className='container-fluid d-flex justify-content-center gap-3'>
+                {printsWithArtist.map(print => (
+                    <CardPrint key={print.id} print={print} user={print.user} />
+                ))}
 
 
 
 
-        </section>
+            </section>
         </div>
     )
 }
